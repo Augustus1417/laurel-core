f3f276f633e3015ca5bf688f64738b3c2ea733ff
Augustus1417 <jedxyril.cruz@gmail.com>
2025-10-17 00:31:16 +0800

    added EOF token type

--- laurel/lrl_token.py
+++ laurel/lrl_token.py
@@ -1,6 +1,5 @@
 from enum import Enum
 
-# Tokens
 class Type(Enum):
     INT = 'INT'
     FLOAT = 'FLOAT'
@@ -10,12 +9,29 @@ class Type(Enum):
     DIV = 'DIV'
     LPAREN = 'LPAREN'
     RPAREN = 'RPAREN'
+    EOF = 'EOF'
+
 
 class Token:
-    def __init__(self, type_:Type, value=None):
+    def __init__(self, type_: Type, value=None, pos_start=None, pos_end=None):
         self.type = type_
         self.value = value
-    
+
+        if pos_start:
+            self.pos_start = pos_start.copy()
+        else:
+            self.pos_start = None
+
+        if pos_end:
+            self.pos_end = pos_end.copy()
+        elif pos_start:
+            # Default: token spans a single character
+            self.pos_end = pos_start.copy()
+            self.pos_end.advance(None)
+        else:
+            self.pos_end = None
+
     def __repr__(self):
-        if self.value: return f'{self.type}:{self.value}'
+        if self.value is not None:
+            return f'{self.type}:{self.value}'
         return f'{self.type}'
